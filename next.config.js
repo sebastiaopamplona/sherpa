9ac784c9eb6ad7bf0b4fe02fcbe47bb87d8f697e
Sebastiao Pamplona <sduartesilvarangelpa@tesla.com>
2022-07-19 14:28:59 +0100

    redirect / to /auth/signin + redirect to /app/* if already signed in

--- next.config.js
+++ next.config.js
@@ -3,7 +3,16 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ["tailwindui.com", "images.unsplash.com"]
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/auth/signin',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,177 +1,189 @@
-import { getProviders, signIn } from "next-auth/react";
+import { getProviders, signIn, useSession } from "next-auth/react";
 
 import { InferGetServerSidePropsType } from "next";
 import { InformationCircleIcon } from "@heroicons/react/outline";
+import { useRouter } from "next/router";
 
 export default function SignIn({
   providers,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  if (status === "authenticated") {
+    router.push("/app/timekeeper");
+  }
+
   return (
     <>
-      <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <img
-            className="mx-auto h-12 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-            alt="Workflow"
-          />
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to your account
-          </h2>
-        </div>
+      {status === "unauthenticated" && (
+        <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt="Workflow"
+            />
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
 
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6" action="#" method="POST">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Email address
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Password
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    id="remember-me"
-                    name="remember-me"
-                    type="checkbox"
-                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                  />
+          <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+              <form className="space-y-6" action="#" method="POST">
+                <div>
                   <label
-                    htmlFor="remember-me"
-                    className="ml-2 block text-sm text-gray-900"
+                    htmlFor="email"
+                    className="block text-sm font-medium text-gray-700"
                   >
-                    Remember me
+                    Email address
                   </label>
-                </div>
-
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-medium text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-              </div>
-
-              <div>
-                <button
-                  type="submit"
-                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    alert(
-                      "Local user login not supported by this application. Please use GitHub OAuth."
-                    );
-                  }}
-                >
-                  Sign in
-                </button>
-              </div>
-            </form>
-
-            <div className="mt-6">
-              <div className="relative">
-                <div className="absolute inset-0 flex items-center">
-                  <div className="w-full border-t border-gray-300" />
-                </div>
-                <div className="relative flex justify-center text-sm">
-                  <span className="px-2 bg-white text-gray-500">
-                    Or continue with
-                  </span>
-                </div>
-              </div>
-              <div className="p-2" />
-              <>
-                {providers
-                  ? Object.values(providers).map((provider, i) => {
-                      if (provider.id !== "email") {
-                        console.log("provider", provider);
-                        return (
-                          <button
-                            key={i}
-                            className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-                            onClick={() =>
-                              signIn(provider.id, {
-                                callbackUrl: `${window.location.origin}/app/timekeeper`,
-                              })
-                            }
-                          >
-                            <span className="sr-only">Sign in with GitHub</span>
-                            <svg
-                              className="w-7 h-7"
-                              aria-hidden="true"
-                              fill="currentColor"
-                              viewBox="0 0 20 20"
-                            >
-                              <path
-                                fillRule="evenodd"
-                                d={oauthProviderToSvg[provider.id]}
-                                clipRule="evenodd"
-                              />
-                            </svg>
-                          </button>
-                        );
-                      }
-                    })
-                  : ""}
-              </>
-
-              <div className="p-2" />
-
-              <div className="rounded-md bg-blue-50 p-4">
-                <div className="flex">
-                  <div className="flex-shrink-0">
-                    <InformationCircleIcon
-                      className="h-5 w-5 text-blue-400"
-                      aria-hidden="true"
+                  <div className="mt-1">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     />
                   </div>
-                  <div className="ml-3 flex-1 md:flex md:justify-between">
-                    <p className=" text-sm text-blue-700">
-                      We currently only support login via GitHub. We plan to
-                      support more providers and local user in the future.
-                    </p>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Password
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center">
+                    <input
+                      id="remember-me"
+                      name="remember-me"
+                      type="checkbox"
+                      className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                    />
+                    <label
+                      htmlFor="remember-me"
+                      className="ml-2 block text-sm text-gray-900"
+                    >
+                      Remember me
+                    </label>
+                  </div>
+
+                  <div className="text-sm">
+                    <a
+                      href="#"
+                      className="font-medium text-indigo-600 hover:text-indigo-500"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                </div>
+
+                <div>
+                  <button
+                    type="submit"
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      alert(
+                        "Local user login not supported by this application. Please use GitHub OAuth."
+                      );
+                    }}
+                  >
+                    Sign in
+                  </button>
+                </div>
+              </form>
+
+              <div className="mt-6">
+                <div className="relative">
+                  <div className="absolute inset-0 flex items-center">
+                    <div className="w-full border-t border-gray-300" />
+                  </div>
+                  <div className="relative flex justify-center text-sm">
+                    <span className="px-2 bg-white text-gray-500">
+                      Or continue with
+                    </span>
+                  </div>
+                </div>
+                <div className="p-2" />
+                <>
+                  {providers
+                    ? Object.values(providers).map((provider, i) => {
+                        if (provider.id !== "email") {
+                          console.log("provider", provider);
+                          return (
+                            <button
+                              key={i}
+                              className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                              onClick={() =>
+                                signIn(provider.id, {
+                                  callbackUrl: `${window.location.origin}/app/timekeeper`,
+                                })
+                              }
+                            >
+                              <span className="sr-only">
+                                Sign in with GitHub
+                              </span>
+                              <svg
+                                className="w-7 h-7"
+                                aria-hidden="true"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                              >
+                                <path
+                                  fillRule="evenodd"
+                                  d={oauthProviderToSvg[provider.id]}
+                                  clipRule="evenodd"
+                                />
+                              </svg>
+                            </button>
+                          );
+                        }
+                      })
+                    : ""}
+                </>
+
+                <div className="p-2" />
+
+                <div className="rounded-md bg-blue-50 p-4">
+                  <div className="flex">
+                    <div className="flex-shrink-0">
+                      <InformationCircleIcon
+                        className="h-5 w-5 text-blue-400"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="ml-3 flex-1 md:flex md:justify-between">
+                      <p className=" text-sm text-blue-700">
+                        We currently only support login via GitHub. We plan to
+                        support more providers and local user in the future.
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
