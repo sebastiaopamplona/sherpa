325356809f80d44fa1945d8a142a4e5fe9a054bd
Sebastiao Pamplona <sduartesilvarangelpa@tesla.com>
2022-07-19 16:36:16 +0100

    add user table + cleanup nextauth server

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,35 +1,48 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-// import { PrismaAdapter } from "@next-auth/prisma-adapter";
-// import { prisma } from "../../../server/db/client";
+import { prisma } from "../../../server/db/client";
 
 export const authOptions: NextAuthOptions = {
-  // adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
-      // profile: (profile) => {
-      //   // Return all the profile information you need.
-      //   // The only truly required field is `id`
-      //   // to be able identify the account when added to a database
-      //   return {
-      //     id: "1",
-      //   };
-      // },
+      authorization: {
+        params: {
+          scope: "user",
+        },
+      },
     }),
   ],
   pages: {
     signIn: "/auth/signin",
   },
-  theme: {
-    colorScheme: "light",
-  },
   callbacks: {
-    async jwt({ token, profile }) {
-      token.userRole = "admin";
+    async signIn({ user }) {
+      const userCount = await prisma.user.count({
+        where: {
+          email: user.email,
+        },
+      });
+
+      if (userCount === 0) {
+        await prisma.user.create({
+          data: {
+            name: user.name,
+            email: user.email,
+            image: user.image,
+          },
+        });
+      }
+
+      return true;
+    },
+    async jwt({ token }) {
       return token;
+    },
+    async session({ session }) {
+      return session;
     },
   },
 };
