c07b1dad0ecce1a6cb0540c599d3de71f08a616a
Sebastiao Pamplona <sduartesilvarangelpa@tesla.com>
2022-07-22 17:16:43 +0100

    clean dashboard page

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,12 +1,7 @@
 import Layout from "../../components/layout"
 import Sidebar from "../../components/sidebar"
-import { trpc } from "../../utils/trpc"
 
 export default function Dashboard() {
-  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }])
-
-  console.log(hello)
-
   return (
     <section>
       <h2>Dashboard</h2>
