3d6bf074c55b07839d0686af4142c932c2a213b0
Sebastiao Pamplona <sduartesilvarangelpa@tesla.com>
2022-07-22 10:32:44 +0100

    add session middleware to trpc app router

--- src/server/createRouter.ts
+++ src/server/createRouter.ts
@@ -1,11 +1,21 @@
+import { TRPCError } from "@trpc/server"
 import { authRouter } from "./router/auth"
 import { createRouter } from "./context"
 import { exampleRouter } from "./router/example"
-// src/server/router/index.ts
+import { storyRouter } from "./router/story"
 import superjson from "superjson"
+// src/server/router/index.ts
 
 export const appRouter = createRouter()
   .transformer(superjson)
+  .middleware(({ ctx, next }) => {
+    if (!ctx.session) {
+      throw new TRPCError({ code: "UNAUTHORIZED" })
+    }
+
+    return next()
+  })
+  .merge("story.", storyRouter)
   .merge("example.", exampleRouter)
   .merge("auth.", authRouter)
 
