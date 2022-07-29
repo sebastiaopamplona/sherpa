7ddfaa3c74587994f627b16ab7e8b00e9e856ad4
Sebastiao Pamplona <sduartesilvarangelpa@tesla.com>
2022-07-29 18:43:49 +0100

    basic story creation working

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -2,19 +2,19 @@ import { UseFormRegisterReturn } from "react-hook-form"
 
 interface Props {
   label: string
+  value?: string
   note?: string
   inputType?: string
-  register: UseFormRegisterReturn<string>
+  register?: UseFormRegisterReturn<string>
 }
 
 export default function Input(props: Props) {
   return (
     <>
-      <label className="block text-sm font-medium text-gray-700">
-        {props.label}
-      </label>
+      <label className="block text-sm font-medium text-gray-700">{props.label}</label>
       <div className="mt-1">
         <input
+          defaultValue={props.value ? props.value : ""}
           type={props.inputType ? props.inputType : "text"}
           className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
           {...props.register}
--- src/components/storyForm/storyForm.tsx
+++ src/components/storyForm/storyForm.tsx
@@ -1,12 +1,13 @@
+import { ButtonDefaultCSS, ButtonDisabledCSS, classNames } from "../../utils/aux"
 import Select, { SelectEntry } from "../../components/select/select"
 import { StoryState as StoryStateEnum, StoryType as StoryTypeEnum } from "@prisma/client"
 import { StoryStates, StoryTypes } from "../../server/data/data"
-import { useEffect, useState } from "react"
+import { StoryType, WorklogType } from "../../server/schemas/schemas"
+import { useEffect, useRef, useState } from "react"
 
-import Button from "../../components/button/button"
 import Input from "../../components/input/input"
-import { StoryType } from "../../server/schemas/schemas"
 import Textarea from "../../components/textarea/textarea"
+import WorklogEntry from "../worklogEntry/worklogEntry"
 import { trpc } from "../../utils/trpc"
 import { useForm } from "react-hook-form"
 import { useRouter } from "next/router"
@@ -37,6 +38,8 @@ export default function StoryForm(props: Props) {
   const [selectedSprint, setSelectedSprint] = useState<SelectEntry>(noSprint)
   const [selectableSprints, setSelectableSprints] = useState<SelectEntry[]>()
 
+  const [isWrittingWorklog, setIsWrittingWorklog] = useState<boolean>(false)
+
   useEffect(() => {
     let statesTmp: SelectEntry[] = []
     let typesTmp: SelectEntry[] = []
@@ -56,7 +59,19 @@ export default function StoryForm(props: Props) {
 
     setSelectableStates(statesTmp)
     setSelectableTypes(typesTmp)
-  }, [])
+
+    if (props.story) {
+      // setSelectedUser({id: props.story.assigneeId, text: props.story.ass})
+      setSelectedType({
+        id: props.story.type,
+        text: StoryTypes.get(props.story.type) as string,
+      })
+      setSelectedState({
+        id: props.story.state,
+        text: StoryStates.get(props.story.state) as string,
+      })
+    }
+  }, [props.story])
 
   const createStoryMutation = trpc.useMutation(["story.create"], {
     onSuccess: (data) => {
@@ -92,11 +107,13 @@ export default function StoryForm(props: Props) {
       onSuccess: (data) => {
         let tmp: SelectEntry[] = [unassignedUser]
         data.map((u) => {
-          tmp.push({
+          const curr = {
             id: u.id,
             text: u.name,
             image: u.image,
-          })
+          }
+          tmp.push(curr)
+          if (props.story && props.story.assigneeId === u.id) setSelectedUser(curr)
         })
         setSelectableUsers(tmp)
       },
@@ -114,15 +131,27 @@ export default function StoryForm(props: Props) {
       onSuccess: (data) => {
         let tmp: SelectEntry[] = []
         data.map((s) => {
-          tmp.push({
+          const curr = {
             id: s.id,
             text: s.title,
-          })
+          }
+          tmp.push(curr)
+          if (props.story && props.story.sprintId === s.id) setSelectedSprint(curr)
         })
         setSelectableSprints(tmp)
       },
     }
   )
+
+  const worklogsRef = useRef<HTMLInputElement | null>(null)
+  const scrollToWorklogs = () => {
+    worklogsRef.current?.scrollIntoView({ block: "center", behavior: "smooth" })
+  }
+
+  const worklogFormRef = useRef<HTMLInputElement | null>(null)
+  const scrollToWorklogForm = () => {
+    worklogFormRef.current?.scrollIntoView({ block: "center", behavior: "smooth" })
+  }
 
   if (users.isLoading || sprints.isLoading) {
     return null
@@ -134,53 +163,162 @@ export default function StoryForm(props: Props) {
         <div>
           <h3 className="text-lg leading-6 font-medium text-gray-900">Create Story</h3>
         </div>
-        <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-          <div className="sm:col-span-5">
-            <Input label="Title" register={register("title")} />
+        <div className="mt-6 max-h-[560px] px-2 overflow-y-scroll grid grid-cols-6 gap-y-6 gap-x-4">
+          <div className="col-span-5">
+            <Input value={props.story ? props.story.title : ""} label="Title" register={register("title")} />
           </div>
           <div className="sm:col-span-1">
-            <Input label="Estimate" inputType="number" register={register("estimate")} />
+            <Input
+              label="Estimate"
+              value={props.story ? props.story.estimate : ""}
+              inputType="number"
+              register={register("estimate")}
+            />
           </div>
-          <div className="sm:col-span-6">
+          <div className="col-span-6">
             <Textarea
               label="Description"
+              value={props.story ? props.story.description : ""}
               register={register("description")}
               note="(Markdown will be supported in the future)"
               nRows={10}
             />
           </div>
-          <div className="sm:col-span-2">
+          <div className="col-span-2">
             {selectedUser && selectableUsers && (
               <Select label="Assigned to" entries={selectableUsers} selectedState={[selectedUser, setSelectedUser]} />
             )}
           </div>
-          <div className="sm:col-span-2">
+          <div className="col-span-2">
             {selectedType && selectableTypes && (
               <Select label="Type" entries={selectableTypes} selectedState={[selectedType, setSelectedType]} />
             )}
           </div>
-          <div className="sm:col-span-2">
+          <div className="col-span-2">
             {selectedState && selectableStates && (
               <Select label="State" entries={selectableStates} selectedState={[selectedState, setSelectedState]} />
             )}
           </div>
-          <div className="sm:col-span-2">
+          <div className="col-span-2">
             {selectedSprint && selectableSprints && (
               <Select label="Sprint" entries={selectableSprints} selectedState={[selectedSprint, setSelectedSprint]} />
             )}
           </div>
-          <div className="sm:col-span-2">
-            <Input label="GitHub Issue Number" note="(Optional)" register={register("githubId")} />
+          <div className="col-span-2">
+            <Input
+              label="GitHub Issue Number"
+              value={props.story ? props.story.githubId : ""}
+              note="(Optional)"
+              register={register("githubId")}
+            />
           </div>
-          <div className="sm:col-span-2">
-            <Input label="Jira Ticket ID" note="(Optional)" register={register("jiraId")} />
+          <div className="col-span-2">
+            <Input
+              label="Jira Ticket ID"
+              value={props.story ? props.story.jiraId : ""}
+              note="(Optional)"
+              register={register("jiraId")}
+            />
           </div>
-          {/* <div className="py-2" /> */}
-          <div className="sm:col-span-6">
-            <Button label={props.story ? "Update story" : "Create story"} onClick={() => {}} />
-            <p> {createStoryMutation.error && createStoryMutation.error.message}</p>
+          <div ref={worklogsRef} className={"col-span-6 inline-flex items-center justify-center text-md font-bold"}>
+            Worklogs
           </div>
-          {/* <div className="py-1" /> */}
+          {props.story && props.story.worklogs.length > 0 ? (
+            <div className="col-span-6 border-2 rounded-sm shadow-sm">
+              <ul role="list" className="divide-y divide-gray-200">
+                {props.story.worklogs.map((worklog: WorklogType) => (
+                  <li
+                    key={worklog.id}
+                    onClick={() => {
+                      // TODO:
+                    }}
+                  >
+                    <WorklogEntry worklog={worklog} />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <>
+              <div
+                className={classNames(
+                  isWrittingWorklog ? "hidden" : "",
+                  "col-span-6 inline-flex items-center justify-center text-md"
+                )}
+              >
+                This story has no worklogs yet.
+              </div>
+            </>
+          )}
+          <div className={classNames(isWrittingWorklog ? "" : "hidden", "col-span-6 grid grid-cols-6 gap-y-6 gap-x-4")}>
+            <div className="col-span-2 col-start-2">
+              <Input label="Worklog effort" />
+            </div>
+            <div className="col-span-2">
+              <Input label="Remaining effort" />
+            </div>
+            <div className="col-span-6">
+              <Textarea
+                value={props.story ? props.story.description : ""}
+                label="What did you do?"
+                register={register("description")}
+                note="(Markdown will be supported in the future)"
+                nRows={10}
+              />
+            </div>
+            <div className="col-span-1 col-start-3 inline-flex items-center justify-center">
+              <button
+                className={ButtonDefaultCSS}
+                onClick={(e) => {
+                  e.preventDefault()
+                  setTimeout(function () {
+                    setIsWrittingWorklog(false)
+                  }, 250)
+                  scrollToWorklogs()
+                }}
+              >
+                Cancel
+              </button>
+            </div>
+            <div className="col-span-1 inline-flex items-center justify-center">
+              <button
+                className={ButtonDefaultCSS}
+                onClick={(e) => {
+                  e.preventDefault()
+                }}
+              >
+                Create
+              </button>
+            </div>
+          </div>
+          <div ref={worklogFormRef} />
+        </div>
+        <div className="mt-6 grid gap-y-6 gap-x-4 grid-cols-6">
+          {props.story ? (
+            <>
+              <div className="col-span-2 col-start-2 inline-flex items-center justify-center">
+                <button className={ButtonDisabledCSS} disabled={true}>
+                  Save changes
+                </button>
+              </div>
+              <div className="col-span-2 inline-flex items-center justify-center">
+                <button
+                  className={ButtonDefaultCSS}
+                  onClick={(e) => {
+                    e.preventDefault()
+                    setIsWrittingWorklog(true)
+                    setTimeout(scrollToWorklogForm, 250)
+                  }}
+                >
+                  Add worklog
+                </button>
+              </div>
+            </>
+          ) : (
+            <div className="col-span-6 inline-flex items-center justify-center">
+              <button className={ButtonDefaultCSS}>Create story</button>
+            </div>
+          )}
         </div>
       </div>
     </form>
