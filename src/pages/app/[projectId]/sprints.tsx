8c96fd8d1b76c95a2bb38559e5dd9cb2c6e7dc7f
Sebastiao Pamplona <sduartesilvarangelpa@tesla.com>
2022-08-05 12:04:02 +0100

    wip: sprints

--- src/pages/app/[projectId]/sprints.tsx
+++ src/pages/app/[projectId]/sprints.tsx
@@ -1,10 +1,49 @@
+import { ArrElement, classNames } from "../../../utils/aux"
+
+import EmptyResources from "../../../components/emptyResources/emptyResources"
 import Layout from "../../../components/layout/layout"
+import { NoSprint } from "../../../server/data/data"
+import Select from "../../../components/select/select"
 import Sidebar from "../../../components/sidebar/sidebar"
+import { SprintGetByProjectIdOutput } from "../../../server/router/sprint"
+import { trpc } from "../../../utils/trpc"
+import { useRouter } from "next/router"
+import { useState } from "react"
 
 export default function Dashboard() {
+  const router = useRouter()
+  const { projectId } = router.query
+
+  const sprints = trpc.useQuery(["sprint.getByProjectId", { projectId: projectId as string }])
+
+  const [selectedSprint, setSelectedSprint] = useState<ArrElement<SprintGetByProjectIdOutput>>(NoSprint)
+
+  if (sprints.isLoading) return null
+
   return (
     <section>
-      <h2>Sprints</h2>
+      <div className="h-full px-[300px]">
+        {sprints.data && sprints.data.length === 0 ? (
+          <div className={classNames(sprints.data && sprints.data.length === 0 ? "" : "hidden")}>
+            <EmptyResources message="You have no sprints in your backlog. Get started by creating one." />
+          </div>
+        ) : (
+          <div className="grid grid-cols-3 content-center gap-[2px]">
+            <div className="col-span-3 flex items-center justify-center py-2">
+              <nav className="relative z-0 inline-flex -space-x-px rounded-md" aria-label="Pagination">
+                <Select
+                  entries={sprints.data!}
+                  getId={(t) => t.id}
+                  getText={(t) => t.title}
+                  selectedState={[selectedSprint, setSelectedSprint]}
+                />
+                <div className="pr-2" />
+              </nav>
+            </div>
+            <div className="col-span-3 flex items-center justify-center py-2"></div>
+          </div>
+        )}
+      </div>
     </section>
   )
 }
--- src/pages/app/[projectId]/timekeeper.tsx
+++ src/pages/app/[projectId]/timekeeper.tsx
@@ -54,7 +54,7 @@ export default function TimeKeeper() {
     <section>
       <div className="h-full px-[100px]">
         <div className="grid grid-cols-11 content-center gap-[2px]">
-          <div className="col-span-11 flex items-end justify-end py-2">
+          <div className="col-span-11 flex items-center justify-center py-2">
             <TimeKeeperNav
               sprints={sprints.data!}
               selectedSprint={selectedSprint}
@@ -163,18 +163,6 @@ const TimeKeeperNav: React.FC<{
       />
       <div className="pr-2" />
       <div
-        className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:cursor-pointer hover:bg-gray-50"
-        onClick={() => {
-          const newCurrDate = new Date()
-          setCurrentDate(newCurrDate)
-          setCurrentDayRange(getWeekBusinessDays(newCurrDate))
-        }}
-      >
-        <span className="sr-only">Previous</span>
-        <IoTodayOutline className="h-5 w-5" />
-      </div>
-      <div className="pr-2" />
-      <div
         className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:cursor-pointer hover:bg-gray-50"
         onClick={() => {
           const newCurrDate = subDays(currentDate, 7)
@@ -201,6 +189,18 @@ const TimeKeeperNav: React.FC<{
       >
         <span className="sr-only">Next</span>
         <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+      </div>
+      <div className="pr-2" />
+      <div
+        className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:cursor-pointer hover:bg-gray-50"
+        onClick={() => {
+          const newCurrDate = new Date()
+          setCurrentDate(newCurrDate)
+          setCurrentDayRange(getWeekBusinessDays(newCurrDate))
+        }}
+      >
+        <span className="sr-only">Previous</span>
+        <IoTodayOutline className="h-5 w-5" />
       </div>
     </nav>
   )
