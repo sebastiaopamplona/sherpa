94d2486d93cd0094e397e4f505af35a6057dbc28
Sebastiao Pamplona <sduartesilvarangelpa@tesla.com>
2022-08-05 09:19:42 +0100

    minor fixes to the sidebar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -63,7 +63,7 @@ export default function Sidebar() {
             <div
               key={item.name}
               className={classNames(
-                router.asPath === item.href
+                router.asPath.includes(item.href)
                   ? "bg-gray-900 text-white"
                   : "text-gray-300 hover:bg-gray-700 hover:text-white",
                 "group flex min-w-fit items-center rounded-md px-2 py-2 text-sm font-medium"
@@ -71,7 +71,7 @@ export default function Sidebar() {
             >
               <item.icon
                 className={classNames(
-                  router.asPath === item.href ? "text-gray-300" : "text-gray-400 group-hover:text-gray-300",
+                  router.asPath.includes(item.href) ? "text-gray-300" : "text-gray-400 group-hover:text-gray-300",
                   "mr-3 h-6 w-6 flex-shrink-0"
                 )}
                 aria-hidden="true"
