9871057b78dfd1525995221d3448daabe997796f
Sebastiao Pamplona <sduartesilvarangelpa@tesla.com>
2022-08-06 13:24:56 +0100

    sprints page wip

---
+++ src/components/sprintForm/sprintForm.tsx
@@ -0,0 +1,93 @@
+import { ButtonDefaultCSS } from "../../utils/aux"
+import DatePicker from "../datepicker/datepicker"
+import Input from "../input/input"
+import { SprintInput } from "../../server/schemas/schemas"
+import Textarea from "../textarea/textarea"
+import { addDays } from "date-fns"
+import { trpc } from "../../utils/trpc"
+import { useForm } from "react-hook-form"
+import { useRouter } from "next/router"
+import { useSession } from "next-auth/react"
+import { useState } from "react"
+
+interface Props {}
+
+export default function SprintForm(props: Props) {
+  const session = useSession()
+  const router = useRouter()
+  const { projectId } = router.query
+
+  const [startAt, setStartAt] = useState<Date>(new Date())
+  const [endAt, setEndAt] = useState<Date>(addDays(new Date(), 14))
+
+  const { handleSubmit, register } = useForm<SprintInput>()
+  const createSprintMutation = trpc.useMutation(["sprint.create"], {
+    onSuccess: () => {
+      //   onCreateOrUpdateSuccess()
+    },
+    onError: () => {
+      //   onCreateOrUpdateError()
+    },
+  })
+  const handleCreateSprint = (values: SprintInput) => {
+    values.creatorId = session?.data?.userid as string
+    values.projectId = projectId
+
+    createSprintMutation.mutate(values)
+  }
+
+  return (
+    <div>
+      <div className="overflow-y-scroll">
+        <form onSubmit={handleSubmit(handleCreateSprint)}>
+          <div className="p-2">
+            <div>
+              <h3 className="text-lg font-medium leading-6 text-gray-900">Create Sprint</h3>
+            </div>
+            <div className="mt-6 grid grid-cols-6 gap-y-6 gap-x-4 overflow-y-scroll px-2 pb-20">
+              <div className="col-span-2">
+                <Input label="Title" register={register("title")} />
+              </div>
+              <div className="col-span-2">
+                <DatePicker
+                  label="Start at"
+                  selectedDateState={[
+                    startAt,
+                    (d: Date) => {
+                      setStartAt(d)
+                    },
+                  ]}
+                />
+              </div>
+              <div className="col-span-2">
+                <DatePicker
+                  label="End at"
+                  selectedDateState={[
+                    endAt,
+                    (d: Date) => {
+                      setEndAt(d)
+                    },
+                  ]}
+                />
+              </div>
+              <div className="col-span-6">
+                <Textarea
+                  label="Description"
+                  value={""}
+                  register={register("description")}
+                  note="(Markdown will be supported in the future)"
+                  nRows={10}
+                />
+              </div>
+            </div>
+            <div className="mt-6 grid grid-cols-6 gap-y-6 gap-x-4">
+              <div className="col-span-6 inline-flex items-center justify-center">
+                <button className={ButtonDefaultCSS}>Create sprint</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
--- src/pages/app/[projectId]/sprints.tsx
+++ src/pages/app/[projectId]/sprints.tsx
@@ -1,10 +1,12 @@
-import { ArrElement, classNames } from "../../../utils/aux"
+import { ArrElement, ButtonDefaultCSS, classNames } from "../../../utils/aux"
 
 import EmptyResources from "../../../components/emptyResources/emptyResources"
 import Layout from "../../../components/layout/layout"
+import Modal from "../../../components/modal/modal"
 import { NoSprint } from "../../../server/data/data"
 import Select from "../../../components/select/select"
 import Sidebar from "../../../components/sidebar/sidebar"
+import SprintForm from "../../../components/sprintForm/sprintForm"
 import { SprintGetByProjectIdOutput } from "../../../server/router/sprint"
 import { trpc } from "../../../utils/trpc"
 import { useRouter } from "next/router"
@@ -17,33 +19,49 @@ export default function Dashboard() {
   const sprints = trpc.useQuery(["sprint.getByProjectId", { projectId: projectId as string }])
 
   const [selectedSprint, setSelectedSprint] = useState<ArrElement<SprintGetByProjectIdOutput>>(NoSprint)
+  const [isSprintsDetailsOpen, setIsSprintDetailsOpen] = useState<boolean>(false)
 
   if (sprints.isLoading) return null
 
   return (
     <section>
       <div className="h-full px-[300px]">
-        {sprints.data && sprints.data.length === 0 ? (
-          <div className={classNames(sprints.data && sprints.data.length === 0 ? "" : "hidden")}>
-            <EmptyResources message="You have no sprints in your backlog. Get started by creating one." />
-          </div>
-        ) : (
-          <div className="grid grid-cols-3 content-center gap-[2px]">
-            <div className="col-span-3 flex items-center justify-center py-2">
-              <nav className="relative z-0 inline-flex -space-x-px rounded-md" aria-label="Pagination">
+        <div className={classNames(sprints.data && sprints.data.length === 0 ? "" : "hidden")}>
+          <EmptyResources message="You have no sprints in your backlog. Get started by creating one." />
+        </div>
+        <div className="grid grid-cols-3 gap-y-6 overflow-hidden bg-white pb-2">
+          <div className="col-span-1 col-start-2 mt-1 flex items-center justify-center">
+            <nav className="relative z-0 inline-flex -space-x-px rounded-md" aria-label="Pagination">
+              <div className={classNames(sprints.data && sprints.data.length === 0 ? "hidden" : "")}>
                 <Select
                   entries={sprints.data!}
                   getId={(t) => t.id}
                   getText={(t) => t.title}
                   selectedState={[selectedSprint, setSelectedSprint]}
                 />
-                <div className="pr-2" />
-              </nav>
-            </div>
-            <div className="col-span-3 flex items-center justify-center py-2"></div>
+              </div>
+              <div className="pr-2" />
+              <button
+                className={ButtonDefaultCSS}
+                onClick={() => {
+                  setIsSprintDetailsOpen(true)
+                }}
+              >
+                Create sprint
+              </button>
+            </nav>
           </div>
-        )}
+          <div className="col-span-3 flex items-center justify-center py-2"></div>
+        </div>
       </div>
+      <Modal
+        isOpen={isSprintsDetailsOpen}
+        onClose={() => {
+          setIsSprintDetailsOpen(false)
+        }}
+      >
+        <SprintForm />
+      </Modal>
     </section>
   )
 }
