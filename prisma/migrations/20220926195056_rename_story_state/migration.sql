34d2c54c1234d20e698e14f9356c97d155271086
Sebastiao Pamplona <sebastiaodsrp@gmail.com>
2022-09-26 21:58:58 +0200

    frontend: make story in progress state appear first + bounce

---
+++ prisma/migrations/20220926195056_rename_story_state/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - The `state` column on the `Story` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- CreateEnum
+CREATE TYPE "StoryState2" AS ENUM ('IN_PROGRESS', 'NEW', 'READY', 'DELIVERED', 'IN_REVIEW', 'DONE', 'DELETED');
+
+-- AlterTable
+ALTER TABLE "Story" DROP COLUMN "state",
+ADD COLUMN     "state" "StoryState2" NOT NULL DEFAULT 'NEW';
+
+-- DropEnum
+DROP TYPE "StoryState";
