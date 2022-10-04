424062c1ef4ebd3dee0c8ff3440abf65b57ccd54
Sebastiao Pamplona <sebastiaodsrp@gmail.com>
2022-10-04 23:58:01 +0100

    wip: sprint dashboard

--- src/pages/app/sprints.tsx
+++ src/pages/app/sprints.tsx
@@ -1,6 +1,5 @@
-import { Area, AreaChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
+import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
 import { ButtonDefaultCSS, classNames } from "../../utils/aux"
-import { useMemo, useState } from "react"
 
 import EmptyResources from "../../components/EmptyResources/EmptyResources"
 import { GetServerSidePropsContext } from "next"
@@ -10,29 +9,80 @@ import SprintForm from "../../components/SprintForm/SprintForm"
 import { checkIfShouldRedirect } from "../../server/aux"
 import { getJourndevAuthSession } from "../../server/session"
 import { useRouter } from "next/router"
+import { useState } from "react"
 
+const data = [
+  {
+    name: "Day 1",
+    ready: 10,
+    in_progress: 0,
+    done: 0,
+  },
+  {
+    name: "Day 2",
+    ready: 7,
+    in_progress: 3,
+    done: 0,
+  },
+  {
+    name: "Day 3",
+    ready: 6,
+    in_progress: 3,
+    done: 1,
+  },
+  {
+    name: "Day 4",
+    ready: 4,
+    in_progress: 3,
+    done: 3,
+  },
+  {
+    name: "Day 5",
+    ready: 4,
+    in_progress: 3,
+    done: 3,
+  },
+  {
+    name: "Day 6",
+    ready: 2,
+    in_progress: 3,
+    done: 5,
+  },
+  {
+    name: "Day 7",
+  },
+  {
+    name: "Day 8",
+  },
+  {
+    name: "Day 9",
+  },
+  {
+    name: "Day 7",
+  },
+]
 export default function Dashboard() {
   const router = useRouter()
   const { projectId, sprintId } = router.query
 
   const [isSprintsDetailsOpen, setIsSprintDetailsOpen] = useState<boolean>(false)
 
-  type dummyEntry = {
-    name: string
-    ready: number
-    started: number
-    delivered: number
-  }
-  const dummyData = useMemo(() => {
-    let tmp: dummyEntry[] = []
-    tmp.push({ name: "Day 0", ready: 10, started: 0, delivered: 0 })
-    for (let i = 0; i < 1 * 8; i++) tmp.push({ name: "", ready: 10, started: 0, delivered: 0 })
-    for (let i = 0; i < 2 * 8; i++) tmp.push({ name: "", ready: 7, started: 3, delivered: 0 })
-    for (let i = 0; i < 1 * 8; i++) tmp.push({ name: "", ready: 5, started: 3, delivered: 2 })
-    for (let i = 0; i < 1 * 8; i++) tmp.push({ name: "", ready: 2, started: 3, delivered: 5 })
-    for (let i = 0; i < 1 * 8; i++) tmp.push({ name: "", ready: 0, started: 0, delivered: 10 })
-    return tmp
-  }, [])
+  // type dummyEntry = {
+  //   name: string
+  //   ready: number
+  //   started: number
+  //   delivered: number
+  // }
+  // const dummyData = useMemo(() => {
+  //   let tmp: dummyEntry[] = []
+  //   tmp.push({ name: "Day 0", ready: 10, started: 0, delivered: 0 })
+  //   for (let i = 0; i < 1 * 8; i++) tmp.push({ name: "", ready: 10, started: 0, delivered: 0 })
+  //   for (let i = 0; i < 2 * 8; i++) tmp.push({ name: "", ready: 7, started: 3, delivered: 0 })
+  //   for (let i = 0; i < 1 * 8; i++) tmp.push({ name: "", ready: 5, started: 3, delivered: 2 })
+  //   for (let i = 0; i < 1 * 8; i++) tmp.push({ name: "", ready: 2, started: 3, delivered: 5 })
+  //   for (let i = 0; i < 1 * 8; i++) tmp.push({ name: "", ready: 0, started: 0, delivered: 10 })
+  //   return tmp
+  // }, [])
 
   return (
     <section>
@@ -60,7 +110,7 @@ export default function Dashboard() {
                 <AreaChart
                   width={500}
                   height={400}
-                  data={dummyData}
+                  data={data}
                   margin={{
                     top: 10,
                     right: 30,
@@ -68,18 +118,16 @@ export default function Dashboard() {
                     bottom: 0,
                   }}
                 >
-                  <CartesianGrid strokeDasharray="1 1" />
+                  <CartesianGrid strokeDasharray="3 3" />
                   <XAxis dataKey="name" />
-                  <YAxis tickCount={10 / 2} interval={0} />
+                  <YAxis />
                   <Tooltip />
-                  <Legend verticalAlign="bottom" height={36} />
-                  <Area type="monotone" dataKey="delivered" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
-                  <Area type="monotone" dataKey="started" stackId="1" stroke="#8884d8" fill="#8884d8" />
+                  <Area type="monotone" dataKey="done" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+                  <Area type="monotone" dataKey="in_progress" stackId="1" stroke="#8884d8" fill="#8884d8" />
                   <Area type="monotone" dataKey="ready" stackId="1" stroke="#ffc658" fill="#ffc658" />
                 </AreaChart>
               </ResponsiveContainer>
             </div>
-            <div className="col-span-3 flex h-[1000px] items-center justify-center bg-red-200 py-2"></div>
           </div>
         ) : (
           <></>
