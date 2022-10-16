ba742a6659191f2a3a8b0c433acd6c7d011bf77f
Hassan Youssef <hasusuf@gmail.com>
2022-10-16 12:24:05 +0200

    reviews

--- prisma/migrations/20221015083547_add_user_related_capacity_table/migration.sql
+++ prisma/migrations/20221015083547_add_user_related_capacity_table/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE "UserRelatedCapacity" (
+CREATE TABLE "UserProjectCapacity" (
     "id" TEXT NOT NULL,
     "capacity" INTEGER NOT NULL DEFAULT 0,
     "date" TIMESTAMP(3) NOT NULL,
@@ -8,11 +8,11 @@ CREATE TABLE "UserRelatedCapacity" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "UserRelatedCapacity_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "UserProjectCapacity_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
-ALTER TABLE "UserRelatedCapacity" ADD CONSTRAINT "UserRelatedCapacity_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "UserProjectCapacity" ADD CONSTRAINT "UserProjectCapacity_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "UserRelatedCapacity" ADD CONSTRAINT "UserRelatedCapacity_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "UserProjectCapacity" ADD CONSTRAINT "UserProjectCapacity_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/server/router/project.ts
+++ src/server/router/project.ts
@@ -7,233 +7,229 @@ import { prisma } from "../db/client"
 import { z } from "zod"
 
 export const projectRouter = createRouter()
-    // CREATE
-    .mutation("create", {
-        input: Project,
-        output: z.object({
-            id: z.string(),
-        }),
-        async resolve({ ctx, input }) {
-            const projectCountByName = await prisma.project.count({
-                where: {
-                    name: input.name,
-                },
-            })
-            if (projectCountByName > 0) {
-                throw new TRPCError({ code: "CONFLICT", message: `Project with name '${input.name}' aleady exists.` })
-            }
-
-            const project = await prisma.project.create({
-                data: {
-                    name: input.name,
-                    description: input.description,
-
-                    githubUrl: input.githubUrl,
-                    jiraUrl: input.jiraUrl,
-
-                    creatorId: input.creatorId,
-                },
-            })
-
-            const adminRole = await prisma.role.findUnique({
-                where: {
-                    name: "admin",
-                },
-            })
-
-            await prisma.userRolesInProjects.create({
-                data: {
-                    userId: input.creatorId,
-                    roleId: adminRole!.id,
-                    projectId: project!.id,
-                },
-            })
-
-            return {
-                id: project.id,
-            }
+  // CREATE
+  .mutation("create", {
+    input: Project,
+    output: z.object({
+      id: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      const projectCountByName = await prisma.project.count({
+        where: {
+          name: input.name,
         },
-    })
+      })
+      if (projectCountByName > 0) {
+        throw new TRPCError({ code: "CONFLICT", message: `Project with name '${input.name}' aleady exists.` })
+      }
 
-    // READ
-    .query("getById", {
-        input: z.object({
-            id: z.string(),
-        }),
-        output: Project,
-        async resolve({ ctx, input }) {
-            const project = await prisma.project.findUnique({
-                where: {
-                    id: input.id,
-                },
-            })
+      const project = await prisma.project.create({
+        data: {
+          name: input.name,
+          description: input.description,
 
-            return {
-                ...project,
-            }
+          githubUrl: input.githubUrl,
+          jiraUrl: input.jiraUrl,
+
+          creatorId: input.creatorId,
         },
-    })
-    .query("getByUserId", {
-        input: z.object({
-            userId: z.string().nullish(),
-        }),
-        async resolve({ ctx, input }) {
-            const userRolesProject = await prisma.userRolesInProjects.findMany({
-                where: {
-                    userId: input.userId as string,
-                },
-            })
+      })
 
-            if (userRolesProject.length === 0) {
-                return []
-            }
-
-            let projectIds: string[] = []
-            userRolesProject.forEach((r) => projectIds.push(r.projectId))
-
-            const projects = await prisma.project.findMany({
-                orderBy: {
-                    name: "asc",
-                },
-                where: {
-                    id: {
-                        in: projectIds,
-                    },
-                },
-                select: {
-                    id: true,
-                    name: true,
-                    description: true,
-                    githubUrl: true,
-                    _count: {
-                        select: {
-                            stories: true,
-                            users: true,
-                            sprints: true,
-                        },
-                    },
-                },
-            })
-
-            return projects
+      const adminRole = await prisma.role.findUnique({
+        where: {
+          name: "admin",
         },
-    })
-    .query("getAll", {
-        output: Projects,
-        async resolve({ ctx, input }) {
-            // TODO(SP): implement paging + filtering
+      })
 
-            const projects = await prisma.project.findMany()
-
-            return projects
+      await prisma.userRolesInProjects.create({
+        data: {
+          userId: input.creatorId,
+          roleId: adminRole!.id,
+          projectId: project!.id,
         },
-    })
+      })
 
-    // UPDATE
-    .mutation("update", {
-        input: Project,
-        output: z.object({
-            id: z.string(),
-        }),
-        async resolve({ ctx, input }) {
-            const project = await prisma.project.update({
-                where: {
-                    id: input.id as string,
-                },
-                data: {
-                    name: input.name,
-                    description: input.description,
+      return {
+        id: project.id,
+      }
+    },
+  })
 
-                    githubUrl: input.githubUrl,
-                    jiraUrl: input.jiraUrl,
-                },
-            })
-
-            return {
-                id: project.id,
-            }
+  // READ
+  .query("getById", {
+    input: z.object({
+      id: z.string(),
+    }),
+    output: Project,
+    async resolve({ ctx, input }) {
+      const project = await prisma.project.findUnique({
+        where: {
+          id: input.id,
         },
-    })
+      })
 
-    // DELETE
-    .mutation("deleteById", {
-        input: z.object({
-            id: z.string(),
-        }),
-        async resolve({ ctx, input }) {
-            await prisma.project.delete({
-                where: {
-                    id: input.id,
-                },
-            })
+      return {
+        ...project,
+      }
+    },
+  })
+  .query("getByUserId", {
+    input: z.object({
+      userId: z.string().nullish(),
+    }),
+    async resolve({ ctx, input }) {
+      const userRolesProject = await prisma.userRolesInProjects.findMany({
+        where: {
+          userId: input.userId as string,
         },
-    })
+      })
 
-    // CREATE user capacity
-    .mutation("createUserCapacity", {
-        input: z.object(
-            {
-                userId: z.string(),
-                projectId: z.string(),
-                capacity: z.number(),
-                date: z.date(),
-            }
-        ),
-        output: z.object({
-            projectId: z.string(),
-        }),
-        async resolve({ ctx, input }) {
-            const m = await prisma.userRelatedCapacity.create({
-                data: {
-                    projectId: input.projectId,
-                    userId: input.userId,
-                    date: input.date,
-                    capacity: input.capacity,
-                },
-            })
+      if (userRolesProject.length === 0) {
+        return []
+      }
 
-            return {
-                projectId: m.projectId,
-            }
+      let projectIds: string[] = []
+      userRolesProject.forEach((r) => projectIds.push(r.projectId))
+
+      const projects = await prisma.project.findMany({
+        orderBy: {
+          name: "asc",
         },
-    })
-
-    // UPDATE user capacity
-    .mutation("updateUserCapacity", {
-        input: z.object(
-            {
-                userId: z.string(),
-                projectId: z.string(),
-                capacity: z.number(),
-                date: z.date(),
-            }
-        ),
-        output: z.object({
-            projectId: z.string(),
-        }),
-        async resolve({ ctx, input }) {
-            const m = await prisma.userRelatedCapacity.update({
-                where: {
-                    projectId_userId_date: {
-                        projectId: input.projectId,
-                        userId: input.userId,
-                        date: input.date,
-                    }
-                },
-                data: {
-                    capacity: input.capacity,
-                },
-            })
-
-            if (!m) {
-                throw new TRPCError({ code: "NOT_FOUND", message: `Capacity not found!` })
-            }
-
-            return {
-                projectId: m.projectId,
-            }
+        where: {
+          id: {
+            in: projectIds,
+          },
         },
-    })
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          githubUrl: true,
+          _count: {
+            select: {
+              stories: true,
+              users: true,
+              sprints: true,
+            },
+          },
+        },
+      })
+
+      return projects
+    },
+  })
+  .query("getAll", {
+    output: Projects,
+    async resolve({ ctx, input }) {
+      // TODO(SP): implement paging + filtering
+
+      const projects = await prisma.project.findMany()
+
+      return projects
+    },
+  })
+
+  // UPDATE
+  .mutation("update", {
+    input: Project,
+    output: z.object({
+      id: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      const project = await prisma.project.update({
+        where: {
+          id: input.id as string,
+        },
+        data: {
+          name: input.name,
+          description: input.description,
+
+          githubUrl: input.githubUrl,
+          jiraUrl: input.jiraUrl,
+        },
+      })
+
+      return {
+        id: project.id,
+      }
+    },
+  })
+
+  // DELETE
+  .mutation("deleteById", {
+    input: z.object({
+      id: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      await prisma.project.delete({
+        where: {
+          id: input.id,
+        },
+      })
+    },
+  })
+
+  // CREATE user capacity
+  .mutation("createUserCapacity", {
+    input: z.object({
+      userId: z.string(),
+      projectId: z.string(),
+      capacity: z.number(),
+      date: z.date(),
+    }),
+    output: z.object({
+      projectId: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      const m = await prisma.userProjectCapacity.create({
+        data: {
+          projectId: input.projectId,
+          userId: input.userId,
+          date: input.date,
+          capacity: input.capacity,
+        },
+      })
+
+      return {
+        projectId: m.projectId,
+      }
+    },
+  })
+
+  // UPDATE user capacity
+  .mutation("updateUserCapacity", {
+    input: z.object({
+      userId: z.string(),
+      projectId: z.string(),
+      capacity: z.number(),
+      date: z.date(),
+    }),
+    output: z.object({
+      projectId: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      const m = await prisma.userProjectCapacity.update({
+        where: {
+          projectId_userId_date: {
+            projectId: input.projectId,
+            userId: input.userId,
+            date: input.date,
+          },
+        },
+        data: {
+          capacity: input.capacity,
+        },
+      })
+
+      if (!m) {
+        throw new TRPCError({ code: "NOT_FOUND", message: `Capacity not found!` })
+      }
+
+      return {
+        projectId: m.projectId,
+      }
+    },
+  })
 
 export type ProjectCreateOutput = inferMutationOutput<"project.create">
 export type ProjectGetAllOutput = inferQueryOutput<"project.getAll">
