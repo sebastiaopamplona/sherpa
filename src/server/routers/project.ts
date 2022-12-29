6a1bd467ec9337f3bfc72f1013e9fcac13d38a65
Sebastiao Pamplona <sebastiaodsrp@gmail.com>
2022-12-29 19:01:38 +0000

    server: delete old trpc router

--- src/server/routers/project.ts
+++
@@ -1,9 +0,0 @@
-import { addBusinessDays, differenceInCalendarDays, isSameDay } from "date-fns"
-import { protectedProcedure, router } from "../trpc"
-
-import { Project } from "../schemas/schemas"
-import { TRPCError } from "@trpc/server"
-import { prisma } from "../db/client"
-import { z } from "zod"
-
-export const projectRouter = t.router({})
