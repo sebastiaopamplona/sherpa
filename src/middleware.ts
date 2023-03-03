37d4061af291d3398e23e291dd133779c73a3bbb
Sebastiao Pamplona <sebastiaodsrp@gmail.com>
2023-03-03 12:58:51 +0000

    middleware test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,20 @@
-export { default } from "next-auth/middleware"
+// export { default } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware"
+
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(req) {
+    console.log("==============")
+    console.log(req.nextauth.token)
+    console.log("==============")
+  },
+  {
+    callbacks: {
+      //   authorized: ({ token }) => true,
+      authorized: ({ token }) => false,
+      // authorized: ({ token }) => token?.role === "admin",
+    },
+  }
+)
 
 export const config = { matcher: ["/app/:path*"] }
